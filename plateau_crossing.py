cd7bb0ed812720459b8f1e536df21ad5c219d6a3
QihanLiu <leo.liuqihan@gmail.com>
2021-09-02 20:01:28 -0400

    add options for K (mut requirement for valley crossing. add dynamic population structure.

--- plateau_crossing.py
+++ plateau_crossing.py
@@ -1,29 +1,39 @@
-#!/usr/bin/env python3
+#! cd $dir && /usr/bin/env python3 $fileName 5e-5 0 0.24
 
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+import itertools as it
+
 
 # get the parameters from the command line:
 parser = argparse.ArgumentParser()
-parser.add_argument("N", type=np.float, help="Population size")
+# parser.add_argument("N", type=np.float, help="Population size")
 parser.add_argument("mut", type=np.float, help="mutation rate")
 parser.add_argument("r", type=np.float, help="frequency of sex")
 parser.add_argument("s", type=np.float, help="advantage of triple mutant")
+parser.add_argument("--k", type=np.int, default=2, help="mutations to valley crossing")
 parser.add_argument("--out", default='./', help="prefix for output files")
 parser.add_argument("--tmax", type=np.float, default=1e8, help="max number of generations to run for")
 parser.add_argument("--tstep", type=np.int, default=10, help="record the population state every tstep generations")
 parser.add_argument("--seed", default=None, type=np.int, help="seed for random number generator")
 args = parser.parse_args()
 
-n = np.uint64(args.N)
+# n = np.uint64(args.N)
 m = args.mut
 r = args.r
 s = args.s
-tmax = np.uint64(args.tmax)
+k = args.k
+dms = pow(2, k)
+nlist = np.loadtxt('cases.csv', delimiter = ',').astype(int)
+# nlist = 5e7 * np.ones(200).astype(int)
+tmax = len(nlist)
+
+# with open(args.out + 'params.txt','w') as outfile:
+# 	print("\n".join(["N = {:.3g}", "mu = {}", "r = {}", "s = {}", "k = {}", "tstep = {}", "seed = {}"]).format(n, m, r, s, k, args.tstep, args.seed), file=outfile)
 
 with open(args.out + 'params.txt','w') as outfile:
-	print("\n".join(["N = {:.3g}", "mu = {}", "r = {}", "s = {}", "tstep = {}", "seed = {}"]).format(n, m, r, s, args.tstep, args.seed), file=outfile)
+	print("\n".join(["mu = {}", "r = {}", "s = {}", "k = {}", "tstep = {}", "seed = {}"]).format(m, r, s, k, args.tstep, args.seed), file=outfile)
 
 
 def multinomial_robust(N, p, eps=1e-7):
@@ -47,12 +57,12 @@ def check_freq(f):
 		return True
 
 
-genotypes = np.array([[-1,-1,-1], [1,-1,-1], [-1,1,-1], [-1,-1,1], [1,1,-1], [1,-1,1], [-1,1,1], [1,1,1]])
-mutation = np.zeros((8,8))
+genotypes = np.asarray(list(it.product((-1,1), repeat=k)))
+mutation = np.zeros((dms,dms))
 
-for i in range(8):
-	for j in range(8):
-		if genotypes[i] @ genotypes[j] == 1:
+for i in range(dms):
+	for j in range(dms):
+		if np.count_nonzero(genotypes[i] - genotypes[j]) == 1:
 			mutation[i,j] = m
 
 def mendel_prob(x, y, z):
@@ -69,20 +79,20 @@ def mendel_prob(x, y, z):
 
 recombination = np.array([[[mendel_prob(x, y, z) for z in genotypes] for y in genotypes] for x in genotypes])
 
-fit_genotype = np.ones(8)
+fit_genotype = np.ones(dms)
 fit_genotype[-1] += s
-freq_genotype = np.zeros(8)
+freq_genotype = np.zeros(dms)
 freq_genotype[0] = 1.0
-num_genotype = np.zeros(8, dtype=np.uint64)
-num_genotype[0] = n
+num_genotype = np.zeros(dms, dtype=np.uint64)
+num_genotype[0] = nlist[0]
 
 np.random.seed(args.seed)
 
 with open(args.out + "trajectory.txt", 'w') as outfile:
 	with open(args.out + "warnings.log", 'w') as logfile:
-		for t in range(tmax):
+		for t in np.arange(tmax - 1) + 1:
 			# recombination:
-			freq_genotype += -r/2 * freq_genotype + r/2 * freq_genotype @ (freq_genotype @ recombination)
+			# freq_genotype += -r/2 * freq_genotype + r/2 * freq_genotype @ (freq_genotype @ recombination)
 			# mutation: 
 			freq_genotype += freq_genotype @ mutation - m * 3 * freq_genotype
 			# selection:
@@ -92,10 +102,10 @@ with open(args.out + "trajectory.txt", 'w') as outfile:
 				print("Warning: improper genotype frequencies ", freq_genotype)
 			# sampling:
 			prvs_gen = num_genotype
-			num_genotype = multinomial_robust(n, freq_genotype)
+			num_genotype = multinomial_robust(nlist[t], freq_genotype)
 			if num_genotype[-1] > 0 and np.sum(prvs_gen[4:]) == 0:
 				print("Warning! Triples appear out of nothing at t={}.".format(t),"\nPrevious generation: ", prvs_gen, "\nPre-sampling frequencies: ", ' '.join(str(x) for x in freq_genotype), "\nNew generation: ", num_genotype, file=logfile)
-			freq_genotype = num_genotype / n
+			freq_genotype = num_genotype / nlist[t]
 	
 			# if triples reach a frequency higher than 1/2, then they have taken over the population:
 			if freq_genotype[-1] > 1/2:
